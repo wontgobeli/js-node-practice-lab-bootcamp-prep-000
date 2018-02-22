c47e99acd562e23d100a1795d6d6d1a33b8964d7
Will Gobeli <willgobeli@gmail.com>
2018-02-22 22:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Will"
-var height = "75"
+var height = 75
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
